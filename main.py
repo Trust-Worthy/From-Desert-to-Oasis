f2dfcd5da7aa1af802347dfde406a0e573b27c66
Trust-Worthy <103729755+Trust-Worthy@users.noreply.github.com>
2023-02-25 16:05:39 -0500

    I can now convert a zip code to a latitude and longitude.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from serpapi import GoogleSearch
+import convert
 
-def mapscall():
+
+
+def mapscall(query, longitute, latitude):
 
     params = {
     "engine": "google_maps",
-    "q": "pizza",
-    "ll": "@40.7455096,-74.0083012,15.1z",
+    "q": query,
+    "ll": "@40.7455096,-74.0083012,15.1z", # this parameter has to be in the form of longitude and latitude
     "type": "search",
-    "api_key": ""
+    "api_key": "c52a3473dc247da70f68ad0adea3b618890bce432d6f30714c438ab9e1e139dd"
     }
 
     search = GoogleSearch(params)
@@ -17,7 +20,8 @@ def mapscall():
 
 
 def main():
-    results = mapscall()
+    results = mapscall("liquor stores")
+    print(results)
 
 
 
